7ec4bcbbb7cc464de2b092e834acfed15bb9fde9
maxime_buguel <maxime.buguel@gmail.com>
2017-01-09 17:17:02 +0100

    fix(packageName) : rename package path

--- jee-checkers-core/src/main/java/fr/isen/checkers/core/CheckersGameImpl.java
+++ jee-checkers-core/src/main/java/fr/isen/checkers/core/CheckersGameImpl.java
@@ -1,4 +1,4 @@
-package src.main.java.fr.isen.checkers.core;
+package fr.isen.checkers.core;
 
 /**
  * Created by Quentin on 09/01/2017.
--- jee-checkers-core/src/test/java/fr/isen/checkers/core/CheckersGameTest.java
+++ jee-checkers-core/src/test/java/fr/isen/checkers/core/CheckersGameTest.java
@@ -1,7 +1,45 @@
 package src.test.java.fr.isen.checkers.core;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 /**
  * Created by Quentin on 09/01/2017.
  */
 public class CheckersGameTest {
+
+    private CheckersGame checkersGame;
+
+    @Before
+    public void init(){
+        //checkersGame =  new CheckersGame();
+        assertThat(true).isEqualTo(true);
+    }
+
+    @Test
+    public void gameIsInitTest(){
+        //checkersGame.init();
+    }
+
+    @Test
+    public void movePawnTest(){
+
+    }
+
+    @Test
+    public void moveLadieTest(){
+
+    }
+
+    @Test
+    public void eatPawnTest(){
+
+    }
+
+    @Test
+    public void isGameWonTest(){
+
+    }
 }
