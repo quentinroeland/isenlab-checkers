ca0e87b3437cc43f9898ec0ef601ab619dfcdc91
Quentin <roelandquentin@gmail.com>
2017-01-15 23:22:05 +0100

    feat(test) : update test

--- jee-checkers-core/src/test/java/fr/isen/checkers/core/CheckersGameTest.java
+++ jee-checkers-core/src/test/java/fr/isen/checkers/core/CheckersGameTest.java
@@ -1,10 +1,9 @@
 package fr.isen.checkers.core;
 
-import fr.isen.checkers.core.board.BoardException;
 import fr.isen.checkers.core.cell.Cell;
 import fr.isen.checkers.core.cell.CellColor;
-import fr.isen.checkers.core.cell.CellContent;
-import fr.isen.checkers.core.cell.CellContentType;
+import fr.isen.checkers.core.exceptions.BoardException;
+import fr.isen.checkers.core.pawns.PawnColor;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -21,21 +20,32 @@ public class CheckersGameTest {
     @Before
     public void init(){
         this.checkersGame =  new CheckersGameImpl();
+        this.checkersGame.init();
     }
 
     @Test
     public void gameIsInitTest(){
-        Cell firstCell = checkersGame.getBoardCell(0, 1);
-        assertThat(firstCell.getColor().equals(CellColor.Dark));
-        CellContent firstCellContent = firstCell.getContent();
-        assertThat(firstCellContent.getType().equals(CellContentType.PAWN));
-        assertThat(firstCellContent.getColor().equals(PawnColor.BLACK));
+        Cell firstCell = checkersGame.getBoardCell(0, 0);
+        assertThat(firstCell.getColor().equals(CellColor.Light));
+        assertThat(firstCell.getPawn()).isNull();
+
+        Cell firstCellWithPawn = checkersGame.getBoardCell(0,1);
+        assertThat(firstCellWithPawn.getColor().equals(CellColor.Dark));
+        assertThat(firstCellWithPawn.getPawn()).isNotNull();
+        assertThat(firstCellWithPawn.getPawn().getColor().equals(PawnColor.BLACK));
+
         Cell lastCell = checkersGame.getBoardCell(9, 9);
         assertThat(lastCell.getColor().equals(CellColor.Light));
-        CellContent lastCellContent = firstCell.getContent();
-        assertThat(lastCellContent.getType().equals(CellContentType.EMPTY));
-        assertThat(lastCellContent.getColor().equals(PawnColor.NULL));
+        assertThat(lastCell.getPawn()).isNull();
+
         assertThatExceptionOfType(BoardException.class).isThrownBy(() -> checkersGame.getBoardCell(10,10));
+
+        Player p1 = checkersGame.getPlayer1();
+        assertThat(p1.getPawns()).hasSize(20);
+
+        Player p2 = checkersGame.getPlayer2();
+        assertThat(p2.getPawns()).hasSize(20);
+
         checkersGame.displayGameBoard();
     }
 
