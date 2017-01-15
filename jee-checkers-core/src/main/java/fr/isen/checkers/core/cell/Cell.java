ffb005f166dc7b684d41ae11e9b0160e8996c0de
Quentin <roelandquentin@gmail.com>
2017-01-15 23:21:22 +0100

    feat(position) : add position class

--- jee-checkers-core/src/main/java/fr/isen/checkers/core/cell/Cell.java
+++ jee-checkers-core/src/main/java/fr/isen/checkers/core/cell/Cell.java
@@ -1,37 +1,76 @@
 package fr.isen.checkers.core.cell;
 
-import fr.isen.checkers.core.PawnColor;
+import fr.isen.checkers.core.Position;
+import fr.isen.checkers.core.pawns.Pawn;
 
 /**
  * Created by Quentin on 09/01/2017.
  */
 public class Cell {
 
-    private CellContent content;
+    private Position position;
     private CellColor color;
+    private Pawn pawn;
 
-    public Cell(CellColor color){
-        this.color = color;
+    public Cell(Integer row, Integer col){
+        this(new Position(row,col));
     }
 
-    public Cell(CellColor color, CellContent content){
-        this.color = color;
-        this.content = content;
+    public Cell(Position p){
+        this.position = p;
+        this.setCellColor();
     }
 
-    public void setColor(CellColor cc){
-        this.color = cc;
+    private void setCellColor() {
+        Integer rowModule = this.position.getRowIndex() % 2;
+        Integer colModule = this.position.getColIndex() % 2;
+        if(rowModule == 0){
+            if(colModule == 0){
+                this.color =  CellColor.Light;
+            }else{
+                this.color =  CellColor.Dark;
+            }
+        }else{
+            if(colModule == 0){
+                this.color =  CellColor.Dark;
+            }else{
+                this.color =  CellColor.Light;
+            }
+        }
     }
 
     public CellColor getColor(){
         return this.color;
     }
 
-    public void setContent(CellContent content) {
-        this.content = content;
+    public Position getPosition() {
+        return position;
     }
 
-    public CellContent getContent() {
-        return content;
+    public void setPawn(Pawn pawn) {
+        this.pawn = pawn;
+    }
+
+    public Pawn getPawn() {
+        return pawn;
+    }
+
+    public boolean hasPawnAtInit() {
+        Boolean response = false;
+
+        Integer rowModule = this.getPosition().getRowIndex() % 2;
+        Integer colModule = this.getPosition().getColIndex() % 2;
+
+        if (rowModule == 0) {
+            if (colModule != 0) {
+                response = true;
+            }
+        } else {
+            if (colModule == 0) {
+               response = true;
+            }
+        }
+
+        return response;
     }
 }
